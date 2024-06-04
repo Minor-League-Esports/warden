73dcf75d687ae5c6e02527f2f19307585d68db85
AnOliveBranch <liv@anolivebranch.dev>
2024-06-03 20:09:05 -0500

    Finish ban & unban commands

--- commands/unban.js
+++ commands/unban.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { EmbedBuilder, SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 const { Logger } = require('../util/Logger.js');
 const { CaseLogger } = require('../util/CaseLogger.js');
 const { opsGuild, opsLogChannelId, caseLogChannelId, guildList } = require('../config.json');
@@ -20,40 +20,49 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 
+		// Force usage of staff server for commands
 		if (interaction.guild.id != opsGuild) {
 			await interaction.editReply('This command must be run from the MLE Staff server');
 			return;
 		}
 
+		// Set up loggers
+		const logChannel = await interaction.client.channels.fetch(opsLogChannelId);
+		const logger = new Logger(logChannel);
+		const caseLogChannel = await interaction.client.channels.fetch(caseLogChannelId);
+		const caseLogger = new CaseLogger(caseLogChannel);
+
+		// Fetch the user
 		const userId = interaction.options.getString('user');
 		interaction.client.users
 			.fetch(userId)
 			.then(async function (user) {
-				const logChannel = await interaction.client.channels.fetch(opsLogChannelId);
-				const logger = new Logger(logChannel);
-				const caseLogChannel = await interaction.client.channels.fetch(caseLogChannelId);
-				const caseLogger = new CaseLogger(caseLogChannel);
-
+				// User exists, begin processing
 				const guildCache = interaction.client.guilds.cache;
 
 				let servers = new Map();
 				let successCount = 0;
 
+				// Loop through all servers
 				for (const guildId of guildList) {
 					const guild = guildCache.get(guildId);
+					// Try to get server
 					if (guild === undefined) {
 						servers.set(guildId, 'Error getting server');
 					} else {
+						// Check for permission
 						if (!guild.members.me.permissions.has(PermissionFlagsBits.BanMembers)) {
 							servers.set(guild.name, 'No permission');
 							continue;
 						}
 
 						try {
+							// Try to unban user
 							await guild.members.unban(user);
 							servers.set(guild.name, 'Success');
 							successCount++;
 						} catch (error) {
+							// Failed to unban
 							if (error.code === 10026) {
 								servers.set(guild.name, 'Not banned');
 							} else {
@@ -67,19 +76,44 @@ module.exports = {
 					}
 				}
 
+				// Check if it succeeded in any servers
 				if (successCount === 0) {
 					await interaction.editReply(
 						`Failed to unban ${user.displayName} from any MLE servers. See case log for details`
 					);
 				} else {
 					await interaction.editReply(
-						`Successfully unbanned ${user.displayName} frorm ${successCount} MLE server${
+						`Successfully unbanned ${user.displayName} from ${successCount} MLE server${
 							successCount === 1 ? '' : 's'
 						}. See case log for details`
 					);
-				}
 
-				caseLogger.logUnban(user, interaction.user, servers);
+					// If it succeeded, send a notice to user
+					const embed = createEmbed();
+					await user
+						.send({ embeds: [embed] })
+						.then(async function () {
+							// Try to send the notice
+							await interaction.followUp(`Successfully sent unban notice to ${user.displayName}`);
+							// Log it
+							caseLogger.logBan(user, interaction.user, servers, 'True');
+						})
+						.catch(async function (error) {
+							// Send failed
+							if (error.code === 50007) {
+								await interaction.followUp(
+									`Failed to send unban notice to ${user.displayName}\nUser has DMs disabled or the bot is blocked`
+								);
+							} else {
+								await interaction.followUp(
+									`Failed to send unban notice to ${user.displayName}, reason unknown`
+								);
+								logger.logMessage(`Error messaging ${user}!\n\`\`\`\n${error}\n\`\`\``);
+							}
+							// Log it
+							caseLogger.logBan(user, interaction.user, servers, 'False');
+						});
+				}
 			})
 			.catch(async function (error) {
 				if (error.code === 10013) {
@@ -91,3 +125,9 @@ module.exports = {
 			});
 	}
 };
+
+return new EmbedBuilder()
+	.setColor('#00ff00')
+	.setTitle(`You have been unbanned`)
+	.setTimestamp()
+	.setDescription(`You have been unbanned from MLE`);
--- events/modalSubmit.js
+++ events/modalSubmit.js
@@ -11,26 +11,33 @@ module.exports = {
 		if (interaction.customId === 'warnModal') {
 			await interaction.deferReply();
 
+			// Fetch the user
 			const userId = interaction.fields.getTextInputValue('userId');
 
 			interaction.client.users
 				.fetch(userId)
 				.then(async function (user) {
+					// Set up loggers
 					const logChannel = await interaction.client.channels.fetch(opsLogChannelId);
 					const logger = new Logger(logChannel);
 					const caseLogChannel = await interaction.client.channels.fetch(caseLogChannelId);
 					const caseLogger = new CaseLogger(caseLogChannel);
 
+					// Create embed
 					const warnText = interaction.fields.getTextInputValue('warnText');
 					const warnEmbed = createEmbed(warnText);
 
+					// Try to send
 					user
 						.send({ embeds: [warnEmbed] })
 						.then(async function () {
+							// Warn success
 							await interaction.editReply(`Successfully warned ${user.displayName}`);
+							// Log it
 							caseLogger.logWarn(user, interaction.user, warnText, 'True');
 						})
 						.catch(async function (error) {
+							// Warn failed
 							if (error.code === 50007) {
 								await interaction.editReply(
 									`Failed to warn ${user.displayName}\nUser has DMs disabled or the bot is blocked`
@@ -40,10 +47,12 @@ module.exports = {
 								logger.logMessage(`Error messaging ${user}!\n\`\`\`\n${error}\n\`\`\``);
 							}
 
+							// Log it
 							caseLogger.logWarn(user, interaction.user, warnText, 'False');
 						});
 				})
 				.catch(async function (error) {
+					// Failed to fetch user
 					if (error.code === 10013) {
 						await interaction.editReply(`Failed to find user with ID ${userId}`);
 					} else {
